ae83bb2e939516c5a24d83743555991f17dc940c
Jason Ziegler <jason@insightfulautomation.com>
2021-04-19 22:19:09 -0500

    added required methods and properties to the Game and Phrase class

--- js/Game.js
+++ js/Game.js
@@ -30,15 +30,50 @@ reduce lives
 */
 
 class Game {
- constructor(lives = 5) {
-     this.phrase = new Phrase();
-     this.lives = lives;
-     this.picks = [];
+ constructor() {
+    this.missed = 0; 
+    this.phrases = ['I love you', 'Eat my shorts', 'Hang loose', 'Cowabunga Dude', 'Fear not'];
+    this.activePhrase = '';
+     
+     
  }
-
+/** This initializes game. */
  startGame() {
     const overlay = document.getElementById('overlay')
     overlay.style.display = 'none';
+    this.activePhrase = this.getRandomPhrase();
+    this.activePhrase.addPhraseToDisplay();
+
  }
+/** This will select a random phrase from the phrases */
+ getRandomPhrase() {
+    return new Phrase(this.phrases[Math.floor(Math.random() * this.phrases.length)]);
+ }
+
+/** This is a description of the foo function. */
+ handleInteraction() {
+
+ }
+
+ /** This will check if all of the letters have been revealed. */
+ checkForWin() {
+
+ }
+
+ /** This will remove a life from the player on screen. */
+removeLife() {
+
+}
+
+/** This end the game and allow the game to be replayed. 
+ * After a game is completed, the gameboard is reset so that clicking the "Start Game" button loads a new game
+ * 
+*/
+gameOver() {
+
+}
+
+
+
 
 }
--- js/Phrase.js
+++ js/Phrase.js
@@ -16,23 +16,22 @@ letter
 
 
 class Phrase {
- constructor() {
-    this.phraseList = ['I love you', 'Eat my shorts', 'Hang loose'];
-    this.phrase = this.pickPhrase();
- }
-
-/** This function will select a random phrase from the phraseList */
- pickPhrase() {
-    return this.phraseList[Math.floor(Math.random() * this.phraseList.length)];
+ constructor(phrase) {
+    
+    this.phrase = phrase;
  }
 
  /** This add the phrase to the game board / display */
  addPhraseToDisplay() {
-
+    console.log('The phrase has been added to the display.')
  }
+ /** This will check if the clicked letter is in the phrase*/
  checkLetter() {
+     console.log('The letter has been checked');
 
  }
-
- showMatchedLetter() { }
+/** This will reveal the letter(s) on the board that matches the player's selection*/
+ showMatchedLetter() { 
+    console.log('The Matched Letter has been shown.');
+ }
 }
